cca3f074bf12fc36809efbaa16ff988d3ebb0eae
plamour <pierre.lamour@edu.univ-fcomte.fr>
2025-10-21 17:58:10 +0200

    Rajout d'un message d'alerte personalisée

--- scripts/copyToClipboard.js
+++
@@ -1,9 +0,0 @@
-function copyToClipboard() {
-    var promoCode = document.getElementById('promoCode');
-    promoCode.type = 'text';
-    promoCode.select();
-    promoCode.setSelectionRange(0, 99999); // Pour appareil tactile est mobile
-    navigator.clipboard.writeText(promoCode.value);
-    promoCode.type = 'hidden';
-    alert("Promo Code copied to clipboard");
-}
